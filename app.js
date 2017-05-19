8a7d130025d70daea4743cef992f4dbdb8a0117f
dan <dgolant@gmail.com>
2017-05-18 23:09:41 -0400

    removed response-builder object
    I never ended up using it

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 
-var responseBuilder = require('./response-builder');
 var dbInterface = require('./db-interface');
 var listEndpoints = require('express-list-endpoints');
 var emailValidator = require("email-validator");
